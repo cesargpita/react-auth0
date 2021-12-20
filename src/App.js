906ff8cb315312ed9a7df77bdcd834f2d68229a9
cesargpita <c.gonzapita@gmail.com>
2021-12-20 19:29:25 +0000

    styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+import { Route } from 'react-router-dom';
+import Home from './Home';
+import Profile from './Profile';
+import Nav from './Nav'
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Nav />
+      <div className="body">
+        <Route path='/' exact component={Home} />
+        <Route path='/profile' exact component={Profile} />
+      </div>
+    </>
   );
 }
 
